96dafde5855fbaf831cac228a1c1d69af6f1c017
LuminDeSilva <luminde9958@gmail.com>
2024-06-27 20:08:24 +0530

    Introduction

---
+++ hello.js
@@ -0,0 +1,13 @@
+//Basic output command
+console.log("Hello for MERN STack");
+
+//Calculating the sum of two numbers
+var a = 10;
+var b = 20;
+var sum = a + b;
+console.log("Sum of a and b is: " + sum);
+
+//var - declare and access can be done globally. Global scope
+//let - declare globally, access  only in the block. Block scope
+//const - declare and access only in the block. Block scope
+
